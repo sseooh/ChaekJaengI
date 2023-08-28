5e7d751902c0e16d315ae33052704862d479f258
sseooh <sseooh@gmail.com>
2023-08-28 19:11:45 +0900

    header 클릭 시 메인으로 이동

--- src/main/resources/static/js/bookReview.js
+++ src/main/resources/static/js/bookReview.js
@@ -55,3 +55,19 @@ function getReview(book) {
     f.submit();
 
 }
+
+// function toMain() {
+//     let f = document.createElement('form');
+//
+//     let obj;
+//     obj = document.createElement('input');
+//     obj.setAttribute('type', 'hidden');
+//     obj.setAttribute('name', 'title')
+//     obj.setAttribute('value', book);
+//
+//     f.appendChild(obj);
+//     f.setAttribute('method', 'post');
+//     f.setAttribute('action', 'reviewList');
+//     document.body.appendChild(f);
+//     f.submit();
+// }
